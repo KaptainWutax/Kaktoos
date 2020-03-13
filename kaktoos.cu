827e083c6ffdad18a48b9c7eca392d8d32650a5a
Bálint Csala <balint.csala@gmail.com>
2020-03-13 14:32:13 +0100

    Quick fix

--- kaktoos.cu
+++ kaktoos.cu
@@ -170,7 +170,7 @@ void setup_gpu_node(GPU_Node* node, int32_t gpu) {
 
 GPU_Node nodes[GPU_COUNT];
 int32_t processed[GPU_COUNT];
-uint64_t offset = 0;
+uint64_t offset = OFFSET;
 uint64_t count = 0;
 std::mutex info_lock;
 
@@ -214,7 +214,7 @@ int main() {
         int timeElapsed = (int)(currentTime - startTime);
         double speed = (double)offset / (double)timeElapsed / 1000000.0;
         printf("Searched %lld seeds, found %lld matches. Time elapsed: %ds. Speed: %.2fm seeds/s. %f%%\n",
-            (long long int)offset, (long long int)count, timeElapsed, speed, (double)offset / TOTAL_WORK_SIZE * 100);
+            (long long int)offset - OFFSET, (long long int)count, timeElapsed, speed, (double)offset / TOTAL_WORK_SIZE * 100);
         std::this_thread::sleep_for(0.5s);
     }
 
